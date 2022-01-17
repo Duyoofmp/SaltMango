a99b35eea1a829a3422a71da383ed1e282cced1d
Haris Wilson <harismoylan@gmail.com>
2022-01-17 12:33:33 +0530

    debug issues

--- functions/api/UserApis/Questions.js
+++ functions/api/UserApis/Questions.js
@@ -6,9 +6,9 @@ const cors = require('cors');
 const app = express();
 app.use(cors({ origin: true }));
 
-const QuestionsFunctions=require('../../service/UserServices/Questions')
-// const common = require("../../common");
-// app.use(common.decodeIDTokenHeader)
+const QuestionsFunctions = require('../../service/UserServices/Questions')
+const common = require("../../common");
+app.use(common.decodeIDTokenHeader)
 
 app.post('/ReadUserQuestions', async (req, res) => QuestionsFunctions.Read(req, res))
 
@@ -17,4 +17,4 @@ app.post('/ReadUserQuestions', async (req, res) => QuestionsFunctions.Read(req, 
 //     memory: "128MB"
 //   }
 
-  exports.UserQuestions = functions.region("asia-south1").https.onRequest(app);
+exports.UserQuestions = functions.region("asia-south1").https.onRequest(app);
--- functions/service/Questions.js
+++ functions/service/Questions.js
@@ -1,79 +1,79 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-const dataHandling=require("../functions");
+const dataHandling = require("../functions");
 const db = admin.firestore();
 //const { database } = require('firebase-functions/v1/firestore');
 
 
-async function Create(req,res){
+async function Create(req, res) {
   try {
-   
-    const temp=[]
-   const   query =await db.collection("Category").where("CategoryName","==",req.body.CategoryName).limit(1).get();
-    let  no=query.docs[0].data().NoOfQuestions;
-   const id=query.docs[0].data().DocId;
-      req.body.Questions.forEach(element => {
-        no=no+1
-        element.index=Date.now();
-        element.QuestionNumber=no
-         temp.push(dataHandling.Create("QuestionsAndAnswers",element))
-         temp.push(dataHandling.Update("Category",{NoOfQuestions:no},id))
-      });
-      await Promise.all(temp)
-       return res.json(true)
+
+    const temp = []
+    const query = await db.collection("Category").where("CategoryName", "==", req.body.CategoryName).limit(1).get();
+    let no = query.docs[0].data().NoOfQuestions;
+    const id = query.docs[0].data().DocId;
+    req.body.Questions.forEach(element => {
+      no = no + 1
+      element.index = Date.now();
+      element.QuestionNumber = no
+      temp.push(dataHandling.Create("QuestionsAndAnswers", element))
+      temp.push(dataHandling.Update("Category", { NoOfQuestions: no }, id))
+    });
+    await Promise.all(temp)
+    return res.json(true)
   } catch (error) {
     console.log(error)
   }
-    
-   
-  }
- async function  Update(req,res){
-    req.body.index=Date.now()
-   await dataHandling.Update("QuestionsAndAnswers",req.body,req.body.DocId)
+
+
+}
+async function Update(req, res) {
+  req.body.index = Date.now()
+  await dataHandling.Update("QuestionsAndAnswers", req.body, req.body.DocId)
   return res.json(true)
-    
-  }
 
- async function Delete(req,res){ 
-   await dataHandling.Delete("QuestionsAndAnswers",req.body.DocId)
-    return res.json(true)
-  }
+}
 
-  async function Read(req,res){
-    try {
-      if(req.body.CategoryName===undefined){
-        const data=await dataHandling.Read("QuestionsAndAnswers",req.body.DocId,req.body.index,req.body.Keyword);
-        return res.json(data)
-      }else{
-        const data=await dataHandling.Read("QuestionsAndAnswers",req.body.DocId,req.body.index,req.body.Keyword,req.body.limit,["CategoryName","==",req.body.CategoryName]);
-        // (data.Options).push(data.Answer);
-        // delete data.Answer
-        return res.json(data)
-      }
-    } catch (error) {
-      functions.logger.error(error)
-      console.log(error)
+async function Delete(req, res) {
+  await dataHandling.Delete("QuestionsAndAnswers", req.body.DocId)
+  return res.json(true)
+}
+
+async function Read(req, res) {
+  try {
+    if (req.body.CategoryName === undefined) {
+      const data = await dataHandling.Read("QuestionsAndAnswers", req.body.DocId, req.body.index, req.body.Keyword);
+      return res.json(data)
+    } else {
+      const data = await dataHandling.Read("QuestionsAndAnswers", req.body.DocId, req.body.index, req.body.Keyword, req.body.limit, ["CategoryName", "==", req.body.CategoryName]);
+      // (data.Options).push(data.Answer);
+      // delete data.Answer
+      return res.json(data)
     }
-   
+  } catch (error) {
+    functions.logger.error(error)
+    console.log(error)
   }
 
-  async function Check(req,res){
-   let msg= await dataHandling.Check("QuestionsAndAnswers",req.body.DocId,req.body.Answer)
-    return res.json(msg)
-  }
+}
 
- 
-  
-  
-  // async function  CreateTeam(obj){
-  //     await dataHandling.Create("Category",obj)
-  //   return true;
-  // }
-  
-  module.exports={
-      Create,
-      Update,
-      Delete,
-      Read,
-      Check
-  }
+async function Check(req, res) {
+  let msg = await dataHandling.Check("QuestionsAndAnswers", req.body.DocId, req.body.Answer)
+  return res.json(msg)
+}
+
+
+
+
+// async function  CreateTeam(obj){
+//     await dataHandling.Create("Category",obj)
+//   return true;
+// }
+
+module.exports = {
+  Create,
+  Update,
+  Delete,
+  Read,
+  Check
+}
