504ca6ac90a70bbee11cef41d92cb839590068ef
Haris Wilson <harismoylan@gmail.com>
2022-01-20 17:37:11 +0530

    geting slot data completed

--- functions/api/UserApis/Home.js
+++ functions/api/UserApis/Home.js
@@ -8,7 +8,7 @@ app.use(cors({ origin: true }));
 
 const HomeFunctions = require('../../service/UserServices/Home')
 const common = require("../../common");
-// app.use(common.decodeIDTokenHeader)
+app.use(common.decodeIDTokenHeader)
 
 // app.post('/ReadDetails', async (req, res) => HomeFunctions.Read(req, res));
 
@@ -21,5 +21,57 @@ app.post('/ReadQuestions', async (req, res) => ReadRandomQuestions(req, res))
 
 app.post('/CheckAnswer', async (req, res) => HomeFunctions.GetPoints(req, res))
 
+app.post('/EnterASlot', async (req, res) => HomeFunctions.EnterASlot(req, res))
+
 
 exports.Home = functions.region("asia-south1").https.onRequest(app);
+
+// token in every api 
+
+// https://asia-south1-salt-mango.cloudfunctions.net/Profile/UpdateProfile
+// Name
+// ReferralCode
+// (Neccessary fields)
+
+// /Profile/CheckReferral
+// ReferralCode
+
+// /Profile/ReadProfile
+// response
+// MyCode
+// SaltCoin
+// Diamond
+// Name
+// ReferralCode
+// (Neccessary fields)
+
+// Home/ReadCategories
+
+// "Available":"India","International",
+// "Keyword":""
+// "DocId":""[send for pagination]
+
+
+// Home/ReadQuestions
+// request 
+//     DocId(from Home/ReadCategories)
+// response
+// [
+//     {
+//         "DocId": "q4jCxapmqSv7WJsGMP2I",
+//         "Question": "qazwsx",
+//         "Options": [
+//             "lkjho",
+//             "qsrgfy",
+//             "asd",
+//             "ojhojho"
+//         ]
+//     }
+// ]
+
+// Home/CheckAnswer
+// request
+// "DocId"
+// "Answer"
+
+// response true/false
--- functions/service/UserServices/Home.js
+++ functions/service/UserServices/Home.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const moment = require("moment-timezone")
 const dataHandling = require("../../functions");
 
 async function Read(req, res) {
@@ -21,9 +22,58 @@ async function GetPoints(req, res) {
 
 }
 
+async function EnterASlot(req, res) {
+    const SlotType = req.body.SlotType;
+    const UserId = req.body.UserId;
+    let DateData;
+
+    switch (SlotType) {
+        case "Daily":
+            DateData = moment().tz('Asia/Kolkata').endOf("day").format("YYYY-MM-DD");
+            break;
+        case "Weekly":
+            DateData = moment().tz('Asia/Kolkata').endOf("week").format("YYYY-MM-DD");
+            break;
+        case "Monthly":
+            DateData = moment().tz('Asia/Kolkata').endOf("month").format("YYYY-MM-DD");
+            break;
+
+        default:
+            DateData = moment().tz('Asia/Kolkata').endOf("day").format("YYYY-MM-DD");
+            break;
+    }
+    const SlotData = await GetSlotData(UserId, SlotType, DateData);
+    if (SlotData.checkSlot) {
+
+
+
+    }
+
+}
+
+async function GetSlotData(UserId, SlotType, DateData) {
+
+    const FreeSlotData = await dataHandling.Read(SlotType, undefined, undefined, undefined, 10, ["UserId", "==", UserId, "Ad", "==", false])
+    const AdSlotData = await dataHandling.Read(SlotType, undefined, undefined, undefined, 15, ["UserId", "==", UserId, "Ad", "==", true])
+    let checkSlot = true;
+
+    if (FreeSlotData.length < 5 && AdSlotData < 5) {
+        checkSlot = false;
+    }
+    return {
+        FreeSlotData,
+        AdSlotData,
+        checkSlot
+    }
+
+}
+
+
+
 module.exports = {
     Read,
-    GetPoints
+    GetPoints,
+    EnterASlot,
 }
 
 
