6f4ef51c4e313d49c4538819e5aa7b0d2395870b
vedanthp16 <pvedanth2@gmail.com>
2022-01-28 23:06:15 +0530

    codecheck

--- functions/triggers/Users.js
+++ functions/triggers/Users.js
@@ -11,11 +11,29 @@ exports.OnUsersCreate = functions.firestore
         const data = change.data()
         const arr = [];
         common.createKeywords(data.Name, arr)
-        let code = common.Keygenerator(4)
-        let ref = docid.substring(0, 3);
-        let refcode = code + ref;
+        Keygenerator()
+        async function Keygenerator() {
+            let generator = '';
+            let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyskiouhjnmbhj'
 
-        const UserData = { DocId: docid, Keywords: arr, MyCode: refcode, SaltCoin: 0, Diamonds: 0 };
+            for (let i = 0; i < 4; i++) {
+                generator += characters.charAt(Math.floor(Math.random() * characters.length))
+            }
+            let array = generator;
+            const code = await admin.firestore().collection("Users").where("MyCode", "==", array).limit(1).get();
+            if (code.size === 0) {
+                let ref = docid.substring(0, 3);
+                let refcode = array + ref;
+                await admin.firestore().collection("Users").doc(docid).update({
+                    MyCode: refcode
+                })
+            }
+            else {
+                Keygenerator()
+            }
+        }
+
+        const UserData = { DocId: docid, Keywords: arr, SaltCoin: 0, Diamonds: 0 };
         if (data.ReferralCode === "" || data.ReferralCode === null || data.ReferralCode === undefined) {
             return db.collection("Users").doc(docid).update(UserData)
         };
@@ -43,20 +61,20 @@ exports.OnUsersUpdate = functions.firestore
         const docid = context.params.docid;
         const data = change.after.data();
         const prevData = change.after.data()
-     const Date=moment().tz('Asia/Kolkata').subtract(30, "d").format("YYYY-MM-DD")
+        const Date = moment().tz('Asia/Kolkata').subtract(30, "d").format("YYYY-MM-DD")
         const arr = [];
         common.createKeywords(data.Name, arr)
         const UserData = { Keywords: arr };
-        if(!common.arrayEquals(data.FriendsList,prevData.FriendsList)){
-            const response=await db.collection("Winners").where("WinDate",">=",Date).where("UserId","==",docid).get()
-                    let batch = firebase.firestore().batch()
-                    response.docs.forEach((doc) => {
-                        const docRef = firebase.firestore().collection("Winners").doc(doc.id)
-                        batch.update(docRef, {FriendsList:data.FriendsList})
-                    })
-                    batch.commit().then(() => {
-                        console.log(`updated all documents inside ${collectionName}`)
-                    })
+        if (!common.arrayEquals(data.FriendsList, prevData.FriendsList)) {
+            const response = await db.collection("Winners").where("WinDate", ">=", Date).where("UserId", "==", docid).get()
+            let batch = firebase.firestore().batch()
+            response.docs.forEach((doc) => {
+                const docRef = firebase.firestore().collection("Winners").doc(doc.id)
+                batch.update(docRef, { FriendsList: data.FriendsList })
+            })
+            batch.commit().then(() => {
+                console.log(`updated all documents inside ${collectionName}`)
+            })
         }
         if (data.ReferralCode === prevData.ReferralCode) {
             return db.collection("Users").doc(docid).update(UserData)
@@ -66,7 +84,7 @@ exports.OnUsersUpdate = functions.firestore
         const CheckReferalUser = await db.collection("Users").where("ReferralCode", "==", data.ReferralCode).limit(1).get()
         if (CheckReferalUser.size === 0) return 0;
         const ReferalUser = CheckReferalUser.docs[0];
-        
+
         // ReferalReward
         const ReferalReward = (await db.doc("Admin/Settings").get()).data().ReferalReward;
 
